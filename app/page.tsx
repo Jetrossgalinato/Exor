bf167903372479a07a0eef1564255ae831682870
Jetross <jetrossneri07@gmail.com>
2026-03-06 15:00:35 +0800

    feat: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { ModeToggle } from "@/components/mode-toggle";
-
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-background">
-      <ModeToggle />
-    </div>
-  );
+  return <div className="min-h-screen bg-background"></div>;
 }
