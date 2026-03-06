2775f91d4e6d60c90be88acd162d7a01e5ee84e2
Jetross <jetrossneri07@gmail.com>
2026-03-06 10:31:26 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <ModeToggle />
+    </div>
+  );
+}
